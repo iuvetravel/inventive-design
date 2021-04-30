633be528f60a9cf551f10b4f3ae7988957b4e496
Calin Puscas <calinpuscas@gmail.com>
2021-04-30 18:01:15 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,4 +7,30 @@ $(document).ready(function () {
     $(".fa-bars").removeClass("fa-times");
     $("nav").removeClass("nav-toggle");
   });
+  $(".count").each(function () {
+    let $this = $(this),
+      countTo = $this.attr("data-count");
+    $({ countNum: $this.text() }).animate(
+      {
+        countNum: countTo,
+      },
+      {
+        duration: 5000,
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum + "+");
+        },
+      }
+    );
+  });
+
+  $(".projects").magnicficPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
 });
